fe5bbcadab5429f0e59702cbee0500307be41c40
Faritor <faritor@unmz.net>
2018-11-30 19:16:05 +0800

    更新

--- src/main/java/net/unmz/java/util/security/AESUtils.java
+++ src/main/java/net/unmz/java/util/security/AESUtils.java
@@ -1,11 +1,15 @@
 package net.unmz.java.util.security;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.SecureRandom;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
 
 /**
  * Project Name:
@@ -18,51 +22,6 @@ import java.security.SecureRandom;
  */
 public class AESUtils {
 
-
-    /**
-     * 加密
-     *
-     * @param content 需要加密的内容
-     * @param password  加密密码
-     * @return
-     */
-    public static byte[] encrypt(String content, String password) {
-        try {
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            kgen.init(256, new SecureRandom(password.getBytes()));
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
-            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
-            byte[] byteContent = content.getBytes("utf-8");
-            cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
-            return cipher.doFinal(byteContent); // 加密
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**解密
-     * @param content  待解密内容
-     * @param password 解密密钥
-     * @return
-     */
-    public static byte[] decrypt(byte[] content, String password) {
-        try {
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            kgen.init(256, new SecureRandom(password.getBytes()));
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
-            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
-            cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
-            return cipher.doFinal(content); // 加密
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 
     /**
      * AES 解密
@@ -84,5 +43,86 @@ public class AESUtils {
         cipher.init(Cipher.DECRYPT_MODE, secKey, new IvParameterSpec(ivArray));// 初始化
         byte[] result = cipher.doFinal(encryptedDataArray);
         return new String(result);
+    }
+
+
+    public static String encryptStr(String content, String password) {
+        return new String(encrypt(content, password));
+    }
+    public static String decryptStr(String cryptograph, String password) {
+        return decrypt(cryptograph.getBytes(),password);
+    }
+
+
+    public static byte[] encrypt(String content, String password) {
+        try {
+            GetAESCipher getAESCipher = new GetAESCipher(password).invoke();
+            SecretKeySpec key = getAESCipher.getKey();
+            Cipher cipher = getAESCipher.getCipher();
+
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] byteContent = content.getBytes(StandardCharsets.UTF_8);
+            byte[] cryptograph = cipher.doFinal(byteContent);
+            return Base64Utils.encode(new String(cryptograph)).getBytes();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static String decrypt(byte[] cryptograph, String password) {
+        try {
+            GetAESCipher getAESCipher = new GetAESCipher(password).invoke();
+            SecretKeySpec key = getAESCipher.getKey();
+            Cipher cipher = getAESCipher.getCipher();
+
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] content = cipher.doFinal(Base64Utils.decode(new String(cryptograph)).getBytes());
+            return new String(content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static byte[] toHash256Deal(String dataStr) {
+        try {
+            MessageDigest digester = MessageDigest.getInstance("SHA-256");
+            digester.update(dataStr.getBytes());
+            byte[] hex = digester.digest();
+            return hex;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+
+    private static class GetAESCipher {
+        private String password;
+        private SecretKeySpec key;
+        private Cipher cipher;
+
+        public GetAESCipher(String password) {
+            this.password = password;
+        }
+
+        public SecretKeySpec getKey() {
+            return key;
+        }
+
+        public Cipher getCipher() {
+            return cipher;
+        }
+
+        public GetAESCipher invoke() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            SecureRandom securerandom = new SecureRandom(toHash256Deal(password));
+            keyGenerator.init(256, securerandom);
+            SecretKey secretKey = keyGenerator.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            key = new SecretKeySpec(enCodeFormat, "AES");
+            Security.addProvider(new BouncyCastleProvider());
+            cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
+            return this;
+        }
     }
 }
